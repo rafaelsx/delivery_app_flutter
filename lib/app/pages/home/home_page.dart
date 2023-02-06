837ccc403218b79c1ff61aa348152d929fab178b
Rafael Simão <rafael.simonx@gmail.com>
2023-02-05 21:22:52 -0300

    Convertendo o homePage para StateFulWidget e criando helper loader utilizando a library "loading_animation_widget".

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,15 +1,28 @@
+import 'package:delivery_app/app/core/ui/helpers/loader.dart';
 import 'package:delivery_app/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:delivery_app/app/models/product_model.dart';
 import 'package:delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DeliveryAppbar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          showLoader();
+          await Future.delayed(Duration(seconds: 2));
+          hideLoader();
+        },
+      ),
       body: Column(
         children: [
           Expanded(
