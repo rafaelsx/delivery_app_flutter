31ef78006570a90f4b89dc535a53dadf1565e336
Rafael Simão <rafael.simonx@gmail.com>
2023-02-05 21:10:42 -0300

    Implementando provider.

---
+++ lib/app/delivery_app.dart
@@ -0,0 +1,23 @@
+import 'package:delivery_app/app/core/provider/application_binding.dart';
+import 'package:delivery_app/app/core/ui/theme/theme_config.dart';
+import 'package:delivery_app/app/pages/home/home_router.dart';
+import 'package:delivery_app/app/pages/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class DeliveryApp extends StatelessWidget {
+  const DeliveryApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ApplicationBinding(
+      child: MaterialApp(
+        title: 'Delivery App',
+        theme: ThemeConfig.theme,
+        routes: {
+          '/': (context) => SplashPage(),
+          '/home': (context) => HomeRouter.page
+        },
+      ),
+    );
+  }
+}
