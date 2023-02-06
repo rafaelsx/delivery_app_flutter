d2b2c1d278ba12b8833a245865edc27acd22788b
Rafael Simão <rafael.simonx@gmail.com>
2023-02-05 21:12:39 -0300

    Criando homePage.

---
+++ lib/app/pages/home/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:delivery_app/app/core/ui/widgets/delivery_appbar.dart';
+import 'package:delivery_app/app/models/product_model.dart';
+import 'package:delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: DeliveryAppbar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return DeliveryProductTile(
+                  product: ProductModel(
+                      id: 0,
+                      name: 'Lanche XYZ',
+                      description: 'Descrição',
+                      price: 20.5,
+                      image:
+                          'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/app/repositories/products/products_repository_impl.dart
@@ -0,0 +1,28 @@
+import 'dart:developer';
+
+import 'package:delivery_app/app/core/exceptions/repository_exception.dart';
+import 'package:delivery_app/app/core/rest_client/custom_dio.dart';
+import 'package:delivery_app/app/models/product_model.dart';
+import 'package:delivery_app/app/repositories/products/products_repository.dart';
+import 'package:dio/dio.dart';
+
+class ProductsRepositoryImpl implements ProductsRepository {
+  final CustomDio dio;
+
+  ProductsRepositoryImpl({
+    required this.dio,
+  });
+
+  @override
+  Future<List<ProductModel>> findAllProducts() async {
+    try {
+      final result = await dio.unauth().get('/products');
+      return result.data
+          .map<ProductModel>((p) => ProductModel.fromMap(p))
+          .toList();
+    } on DioError catch (e, s) {
+      log('Erro ao buscar produtos', error: e, stackTrace: s);
+      throw RepositoryException(message: 'Erro ao buscar produtos');
+    }
+  }
+}
